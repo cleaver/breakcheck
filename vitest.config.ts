672cfcf1c860ea2921b4c434e073e97fef83219f
Cleaver Barnes <295771+cleaver@users.noreply.github.com>
2025-05-07 16:39:39 -0400

    configure vitest for our aliases

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vitest/config";
+import { resolve } from "path";
 
 export default defineConfig({
   test: {
@@ -9,5 +10,14 @@ export default defineConfig({
       reporter: ["text", "json", "html"],
     },
     include: ["src/**/*.test.ts"],
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+      "@api": resolve(__dirname, "./src/api"),
+      "@cli": resolve(__dirname, "./src/cli"),
+      "@core": resolve(__dirname, "./src/core"),
+      "@project-types": resolve(__dirname, "./src/types"),
+    },
   },
 });
