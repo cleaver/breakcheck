df34e06334cba846cc96ac7380d9dc66f400fc4c
Cleaver Barnes <295771+cleaver@users.noreply.github.com>
2025-06-03 16:51:22 -0400

    create initial json rules types

---
+++ src/types/rules.ts
@@ -0,0 +1,89 @@
+/**
+ * Type definitions for Breakcheck JSON Rules
+ * Based on the JSON Rules Specification
+ */
+
+/**
+ * The overall processing mode for the rules
+ */
+export type ProcessingMode = "default_include" | "explicit_include";
+
+/**
+ * The type of selector used in a rule
+ */
+export type SelectorType = "css" | "xpath";
+
+/**
+ * The type of action to perform
+ */
+export type ActionType =
+  | "include"
+  | "exclude"
+  | "remove_attr"
+  | "rewrite_attr"
+  | "rewrite_content";
+
+/**
+ * Base interface for action modifiers
+ */
+interface BaseActionModifiers {
+  content_regex?: string;
+}
+
+/**
+ * Modifiers for remove_attr action
+ */
+interface RemoveAttrModifiers extends BaseActionModifiers {
+  attr: string;
+}
+
+/**
+ * Modifiers for rewrite_attr action
+ */
+interface RewriteAttrModifiers extends BaseActionModifiers {
+  attr: string;
+  regex: string;
+  replace: string;
+}
+
+/**
+ * Modifiers for rewrite_content action
+ */
+interface RewriteContentModifiers extends BaseActionModifiers {
+  regex: string;
+  replace: string;
+}
+
+/**
+ * Union type for all possible action modifiers
+ */
+export type ActionModifiers =
+  | RemoveAttrModifiers
+  | RewriteAttrModifiers
+  | RewriteContentModifiers
+  | BaseActionModifiers;
+
+/**
+ * Represents a single action to be performed
+ */
+export interface Action {
+  action: ActionType;
+  modifiers?: ActionModifiers;
+}
+
+/**
+ * Represents a single rule with a selector and its actions
+ */
+export interface Rule {
+  selector_type: SelectorType;
+  selector: string;
+  actions: Action[];
+}
+
+/**
+ * The root structure of the JSON rules document
+ */
+export interface RulesDocument {
+  mode: ProcessingMode;
+  rules: Rule[];
+}
