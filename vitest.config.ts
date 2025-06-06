c39e20858deb465e87a37b73d0b8a786ad19d4ff
Cleaver Barnes <295771+cleaver@users.noreply.github.com>
2025-06-06 09:43:41 -0400

    exclude test servers from coverage report

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vitest/config";
+import { coverageConfigDefaults, defineConfig } from "vitest/config";
 import { resolve } from "path";
 
 export default defineConfig({
@@ -8,6 +8,7 @@ export default defineConfig({
     coverage: {
       provider: "v8",
       reporter: ["text", "json", "html"],
+      exclude: [...coverageConfigDefaults.exclude, "src/__test_server__/**/*"],
     },
     include: ["src/**/*.test.ts"],
   },
